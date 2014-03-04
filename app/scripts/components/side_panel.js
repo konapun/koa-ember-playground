3324ececcf6571001154fae451b857a0fdaa0213
konapun <bremen.braun@gmail.com>
2014-03-03 22:14:12 -0700

    Tried to get custom component working

---
+++ app/scripts/components/side_panel.js
@@ -0,0 +1,11 @@
+KoaEmberPlayground.SidePanelComponent = Em.Component.extend({
+  tagName: 'div',
+  classNames: ['sidepanel', 'whatever'],
+  classNameBindings: ['isOpen:opened:closed'],
+  isOpen: false,
+  actions: {
+    toggleSidepanel: function(){
+        this.toggleProperty('isOpen');
+    }
+  }
+});
